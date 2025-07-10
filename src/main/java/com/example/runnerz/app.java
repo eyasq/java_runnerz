7433b3636b99d657c1d3e7a4e4fc11b3157517e6
eyasq <qeremeyas99@gmail.com>
2025-07-10 15:42:23 +0300

    slow progress

--- src/main/java/com/example/runnerz/app.java
+++ src/main/java/com/example/runnerz/app.java
@@ -1,11 +1,17 @@
 package com.example.runnerz;
 
+import com.example.runnerz.run.Location;
+import com.example.runnerz.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class app {
@@ -15,9 +21,15 @@ public class app {
 
 	public static void main(String[] args) {
 		SpringApplication.run(app.class, args);
-		log.info("Something changed");
+	}
+	@Bean
+	CommandLineRunner runner(){
+		return args ->{
+			Run run = new Run(1, "Morning Run", LocalDateTime.now().minusHours(1),
+					LocalDateTime.now(),5, Location.OUTDOOR);
+			log.info("Run:" + run);
 
-
+		};
 	}
 
 }
--- src/main/java/com/example/runnerz/run/Run.java
+++ src/main/java/com/example/runnerz/run/Run.java
@@ -1,96 +1,14 @@
 package com.example.runnerz.run;
 
-import org.springframework.cglib.core.Local;
-
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-public class Run {
-    private Integer id;
-    private String title;
-    private LocalDateTime startedOn;
-    private LocalDateTime completedOn;
-    private Integer miles;
-    private Location location;
+public record Run(
+        Integer id,
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        Integer miles,
+         Location location)
+{
 
-
-    public Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles, Location location){
-        this.id = id;
-        this.title = title;
-        this.startedOn = startedOn;
-        this.completedOn = completedOn;
-        this.location = location;
-        this.miles = miles;
-
-        if(!completedOn.isAfter(startedOn)){
-            throw new IllegalArgumentException("Completed on must be after started on.");
-        }
-    }
-
-    public Duration getDuration(){
-        return Duration.between(startedOn, completedOn);
-    }
-
-    public Integer getAvgPace(){
-        return Math.toIntExact(getDuration().toMinutes()/miles);
-    }
-    public Integer getId(){
-        return id;
-    }
-    public String getTitle(){
-        return title;
-    }
-    public LocalDateTime getStartedOn(){
-        return startedOn;
-    }
-    public LocalDateTime getCompletedOn(){
-        return completedOn;
-    }
-
-    public Integer getMiles(){
-        return miles;
-    }
-    public void setId(Integer id){
-        this.id = id;
-    }
-    public void setTitle(String title){
-        this.title = title;
-    }
-    public void setStartedOn(LocalDateTime startedOn){
-        this.startedOn = startedOn;
-    }
-    public void setCompletedOn(LocalDateTime completedOn){
-        this.completedOn = completedOn;
-    }
-    public void setMiles(Integer miles){
-        this.miles=miles;
-    }
-    public void setLocation(Location location){
-        this.location = location;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Run run = (Run) o;
-        return Objects.equals(id, run.id) && Objects.equals(title, run.title) && Objects.equals(startedOn, run.startedOn) && Objects.equals(completedOn, run.completedOn) && Objects.equals(miles, run.miles) && Objects.equals(location, run.location);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, startedOn, completedOn, miles, location);
-    }
-
-    @Override
-    public String toString() {
-        return "Run{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", startedOn=" + startedOn +
-                ", completedOn=" + completedOn +
-                ", miles=" + miles +
-                ", location=" + location +
-                '}';
-    }
 }
